a14e21071e4f0253cdd7e916765588515f222c69
Andrew Clubb <andrew.clubb@eagles.oc.edu>
2023-01-11 13:27:54 -0600

    first files

---
+++ routes/course.routes.js
@@ -0,0 +1,21 @@
+module.exports = app => {
+    const course = require("../controllers/course.controller.js");
+    var router = require("express").Router();
+
+    // Create a new course
+    router.post("/", course.create);
+    // Retrieve all courses
+    router.get("/", course.findAllCourses);
+    // Retrieve a single course with id
+    router.get("/id/:id", course.findCourseById);
+    // Retrieve courses by dept
+    router.get("/dept/:dept", course.findCoursesByDept);
+    // Retrieve courses by name
+    router.get("/name/:name", course.findCoursesByName);
+    // Update a course with id
+    router.put("/:id", course.update);
+    // Delete a course with id
+    router.delete("/:id", course.delete);
+
+    app.use('/course-t2', router);
+};
