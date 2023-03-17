ca25aba92e762f555b6e26e2cf2545724598f5a9
caspcole <caspiancoleman@gmail.com>
2023-03-17 16:46:33 -0500

    added get songs by composer id

--- app/controllers/song.controller.js
+++ app/controllers/song.controller.js
@@ -7,30 +7,29 @@ exports.create = (req, res) => {
   // Validate request
   if (!req.body.title) {
     res.status(400).send({
-      message: "title can not be empty!"
+      message: "title can not be empty!",
     });
     return;
   } else if (!req.body.composerId) {
     res.status(400).send({
-      message: "composerId can not be empty!"
+      message: "composerId can not be empty!",
     });
     return;
   }
-  
+
   const song = {
     title: req.body.title,
-    composerId: req.body.composerId
+    composerId: req.body.composerId,
   };
 
   // Create and Save a new song
   Song.create(song)
-    .then(data => {
+    .then((data) => {
       res.send(data);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message:
-          err.message || "Some error occurred while creating the song."
+        message: err.message || "Some error occurred while creating the song.",
       });
     });
 };
@@ -38,13 +37,12 @@ exports.create = (req, res) => {
 // Retrieve all songs from the database
 exports.findAll = (req, res) => {
   Song.findAll()
-    .then(data => {
+    .then((data) => {
       res.send(data);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message:
-          err.message || "Some error occurred while retrieving songs."
+        message: err.message || "Some error occurred while retrieving songs.",
       });
     });
 };
@@ -53,18 +51,42 @@ exports.findAll = (req, res) => {
 exports.findById = (req, res) => {
   const id = req.params.id;
   Song.findByPk(id)
-    .then(data => {
+    .then((data) => {
       if (data) {
         res.send(data);
       } else {
         res.status(404).send({
-          message: 'Cannot find song with id=' + id
+          message: "Cannot find song with id=" + id,
         });
       }
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message: 'Error retrieving song with id=' + id
+        message: "Error retrieving song with id=" + id,
+      });
+    });
+};
+
+// Retrieve all songs from the database with composer id
+exports.findByComposerId = (req, res) => {
+  const id = req.params.id;
+  Song.findAll({
+    where: {
+      composerId: id,
+    },
+  })
+    .then((data) => {
+      if (data) {
+        res.send(data);
+      } else {
+        res.status(404).send({
+          message: "Cannot find songs with ComposerId=" + id,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while retrieving songs.",
       });
     });
 };
@@ -73,48 +95,54 @@ exports.findById = (req, res) => {
 exports.update = (req, res) => {
   const id = req.params.id;
   Song.update(req.body, {
-    where: { id: id }
+    where: { id: id },
   })
-  .then(num => {
-    if (num == 1) {
-      res.send({
-        message: 'Song was updated successfully.'
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: "Song was updated successfully.",
+        });
+      } else {
+        res.send({
+          message:
+            "Cannot update song with id=" +
+            id +
+            ". Maybe the song was not found or req.body is empty!",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error updating song with id=" + id,
       });
-    } else {
-      res.send({
-        message: 'Cannot update song with id=' + id + '. Maybe the song was not found or req.body is empty!'
-      });
-    }
-  })
-  .catch(err => {
-    res.status(500).send({
-      message: 'Error updating song with id=' + id
     });
-  });
 };
 
 // Delete a(n) song with the specified id in the request
 exports.delete = (req, res) => {
   const id = req.params.id;
   Song.destroy({
-    where: { id: id }
+    where: { id: id },
   })
-  .then(num => {
-    if (num == 1) {
-      res.send({
-        message: 'Song was deleted successfully!'
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: "Song was deleted successfully!",
+        });
+      } else {
+        res.send({
+          message:
+            "Cannot delete song with id=" +
+            id +
+            ". Maybe the song was not found",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Could not delete song with id=" + id,
       });
-    } else {
-      res.send({
-        message: 'Cannot delete song with id=' + id + '. Maybe the song was not found'
-      })
-    }
-  })
-  .catch((err) => {
-    res.status(500).send({
-      message: "Could not delete song with id=" + id,
     });
-  });
 };
 
 // Delete all songs from the database.
@@ -128,8 +156,7 @@ exports.deleteAll = (req, res) => {
     })
     .catch((err) => {
       res.status(500).send({
-        message:
-          err.message || "Some error occurred while removing all songs.",
+        message: err.message || "Some error occurred while removing all songs.",
       });
     });
 };
