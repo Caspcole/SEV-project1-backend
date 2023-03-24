3b082b8a609d34ece18188487fe6c93f3b819a15
ThatGremlin <ben.johnson@eagles.oc.edu>
2023-03-24 14:11:30 -0500

    ReMerge

--- app/controllers/eventTimeslot.controller.js
+++ app/controllers/eventTimeslot.controller.js
@@ -7,39 +7,40 @@ exports.create = (req, res) => {
   // Validate request
   if (!req.body.startTime) {
     res.status(400).send({
-      message: "start time can not be empty!"
+      message: "start time can not be empty!",
     });
     return;
   } else if (!req.body.endTime) {
     res.status(400).send({
-      message: "end time can not be empty!"
+      message: "end time can not be empty!",
     });
     return;
   } else if (!req.body.eventId) {
     res.status(400).send({
-      message: "eventId can not be empty!"
+      message: "eventId can not be empty!",
     });
     return;
   }
-  
+
   const eventTimeslot = {
     startTime: req.body.startTime,
     endTime: req.body.endTime,
     hasPassed: req.body.hasPassed,
     isComplete: req.body.isComplete,
     accompanistId: req.body.accompanistId,
-    eventId: req.body.eventId
+    eventId: req.body.eventId,
   };
 
   // Create and Save a new eventTimeslot
   EventTimeslot.create(eventTimeslot)
-    .then(data => {
+    .then((data) => {
       res.send(data);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while creating the eventTimeslot."
+          err.message ||
+          "Some error occurred while creating the eventTimeslot.",
       });
     });
 };
@@ -47,13 +48,13 @@ exports.create = (req, res) => {
 // Retrieve all eventTimeslots from the database
 exports.findAll = (req, res) => {
   EventTimeslot.findAll()
-    .then(data => {
+    .then((data) => {
       res.send(data);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while retrieving eventTimeslots."
+          err.message || "Some error occurred while retrieving eventTimeslots.",
       });
     });
 };
@@ -62,18 +63,18 @@ exports.findAll = (req, res) => {
 exports.findById = (req, res) => {
   const id = req.params.id;
   EventTimeslot.findByPk(id)
-    .then(data => {
+    .then((data) => {
       if (data) {
         res.send(data);
       } else {
         res.status(404).send({
-          message: 'Cannot find eventTimeslot with id=' + id
+          message: "Cannot find eventTimeslot with id=" + id,
         });
       }
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message: 'Error retrieving eventTimeslot with id=' + id
+        message: "Error retrieving eventTimeslot with id=" + id,
       });
     });
 };
@@ -83,21 +84,21 @@ exports.findByEventId = (req, res) => {
   const id = req.params.id;
   EventTimeslot.findAll({
     where: {
-      eventId: id
-    }
+      eventId: id,
+    },
   })
-    .then(data => {
+    .then((data) => {
       if (data) {
         res.send(data);
       } else {
         res.status(404).send({
-          message: 'Cannot find eventTimeslots with EventId=' + id
+          message: "Cannot find eventTimeslots with EventId=" + id,
         });
       }
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message: 'Error retrieving eventTimeslot with EventId=' + id
+        message: "Error retrieving eventTimeslot with EventId=" + id,
       });
     });
 };
@@ -106,48 +107,54 @@ exports.findByEventId = (req, res) => {
 exports.update = (req, res) => {
   const id = req.params.id;
   EventTimeslot.update(req.body, {
-    where: { id: id }
+    where: { id: id },
   })
-  .then(num => {
-    if (num == 1) {
-      res.send({
-        message: 'EventTimeslot was updated successfully.'
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: "EventTimeslot was updated successfully.",
+        });
+      } else {
+        res.send({
+          message:
+            "Cannot update eventTimeslot with id=" +
+            id +
+            ". Maybe the eventTimeslot was not found or req.body is empty!",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error updating eventTimeslot with id=" + id,
       });
-    } else {
-      res.send({
-        message: 'Cannot update eventTimeslot with id=' + id + '. Maybe the eventTimeslot was not found or req.body is empty!'
-      });
-    }
-  })
-  .catch(err => {
-    res.status(500).send({
-      message: 'Error updating eventTimeslot with id=' + id
     });
-  });
 };
 
 // Delete a(n) eventTimeslot with the specified id in the request
 exports.delete = (req, res) => {
   const id = req.params.id;
   EventTimeslot.destroy({
-    where: { id: id }
+    where: { id: id },
   })
-  .then(num => {
-    if (num == 1) {
-      res.send({
-        message: 'EventTimeslot was deleted successfully!'
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: "EventTimeslot was deleted successfully!",
+        });
+      } else {
+        res.send({
+          message:
+            "Cannot delete eventTimeslot with id=" +
+            id +
+            ". Maybe the eventTimeslot was not found",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Could not delete eventTimeslot with id=" + id,
       });
-    } else {
-      res.send({
-        message: 'Cannot delete eventTimeslot with id=' + id + '. Maybe the eventTimeslot was not found'
-      })
-    }
-  })
-  .catch((err) => {
-    res.status(500).send({
-      message: "Could not delete eventTimeslot with id=" + id,
     });
-  });
 };
 
 // Delete all eventTimeslots from the database.
@@ -157,12 +164,178 @@ exports.deleteAll = (req, res) => {
     truncate: false,
   })
     .then((nums) => {
-      res.send({ message: `${nums} eventTimeslots were deleted successfully!` });
+      res.send({
+        message: `${nums} eventTimeslots were deleted successfully!`,
+      });
     })
     .catch((err) => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while removing all eventTimeslots.",
+          err.message ||
+          "Some error occurred while removing all eventTimeslots.",
+      });
+    });
+};
+
+exports.getEventCritiquesBySemesterId = (req, res) => {
+  const months = [
+    "January",
+    "Febuary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  Event.findAll({
+    where: {
+      semesterId: { [Op.eq]: req.params.semesterId },
+    },
+    attributes: ["id", "date", "type"],
+    include: {
+      model: db.eventTimeslot,
+      required: true,
+      attributes: ["id"],
+      include: {
+        model: db.studentTimeslot,
+        required: true,
+        attributes: ["id"],
+        include: [
+          {
+            model: db.critique,
+            required: true,
+            attributes: ["id", "type", "grade", "comment"],
+            include: {
+              model: db.userRole,
+              required: true,
+              attributes: ["id", "title"],
+              include: {
+                model: db.user,
+                required: true,
+                attributes: ["id", "fName", "lName"],
+              },
+            },
+          },
+          {
+            model: db.studentInstrument,
+            required: true,
+            attributes: ["id", "studentId"],
+            include: [
+              {
+                model: db.userRole,
+                required: true,
+                as: "student",
+                attributes: ["id", "title"],
+                include: {
+                  model: db.user,
+                  required: true,
+                  attributes: ["id", "fName", "lName"],
+                },
+              },
+              {
+                model: db.instrument,
+                required: true,
+                attributes: ["id", "name"],
+              },
+            ],
+          },
+        ],
+      },
+    },
+  })
+    .then((data) => {
+      let text = "[";
+      //for each event
+      for (let eI = 0; eI < data.length; eI++) {
+        let curEvent = data[eI].dataValues;
+        //for each event timeslot
+        for (let etI = 0; etI < curEvent.eventTimeslots.length; etI++) {
+          let curEventTs = curEvent.eventTimeslots[etI].dataValues;
+          //for each student timeslot
+          for (let stI = 0; stI < curEventTs.studentTimeslots.length; stI++) {
+            let curStudentTs = curEventTs.studentTimeslots[stI].dataValues;
+            let student =
+              curStudentTs.studentInstrument.dataValues.student.dataValues;
+            let critiquerArray = [];
+            text +=
+              '{"eventDate":"' +
+              months[Number(curEvent.date.substring(5, 7) - 1)] +
+              " " +
+              curEvent.date.substring(8) +
+              '","eventType":"' +
+              curEvent.type +
+              '","studentTitle":"' +
+              student.title +
+              '","studentFName":"' +
+              student.user.dataValues.fName +
+              '","studentLName":"' +
+              student.user.dataValues.lName +
+              '","studentInstrument":"';
+            text +=
+              curStudentTs.studentInstrument.dataValues.instrument.dataValues
+                .name + '","critiquers":[';
+            //for each critique
+            for (let cI = 0; cI < curStudentTs.critiques.length; cI++) {
+              let curCritique = curStudentTs.critiques[cI].dataValues;
+              if (critiquerArray.length == 0) {
+                text +=
+                  '{"critiquerName":"' +
+                  (curCritique.userRole.dataValues.title === null
+                    ? ""
+                    : curCritique.userRole.dataValues.title + " ") +
+                  curCritique.userRole.dataValues.user.dataValues.fName +
+                  " " +
+                  curCritique.userRole.dataValues.user.dataValues.lName +
+                  '","comments":[';
+                critiquerArray.push(curCritique.userRole.dataValues.id);
+              } else if (
+                critiquerArray.includes(curCritique.userRole.dataValues.id)
+              ) {
+                //same faculty
+                text += ",";
+              } else {
+                //new faculty
+                critiquerArray.push(curCritique.userRole.dataValues.id);
+                text +=
+                  ']},{"critiquerName":"' +
+                  (curCritique.userRole.dataValues.title === null
+                    ? ""
+                    : curCritique.userRole.dataValues.title + " ") +
+                  curCritique.userRole.dataValues.user.dataValues.fName +
+                  " " +
+                  curCritique.userRole.dataValues.user.dataValues.fName +
+                  '","comments":[';
+              }
+              text +=
+                '{"critiqueTitle":"' +
+                curCritique.type +
+                '","critiqueComment":"' +
+                curCritique.comment +
+                '","critiqueGrade":"' +
+                curCritique.grade +
+                '"}';
+            }
+            text += "]}]}";
+          }
+          if (curEvent.eventTimeslots.length - etI > 1) {
+            text += ",";
+          }
+        }
+        if (data.length - eI > 1) {
+          text += ",";
+        }
+      }
+      text += "]";
+      res.send(JSON.parse(text));
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while retrieving events.",
       });
     });
 };
