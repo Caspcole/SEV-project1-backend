56417cdbdb3a88ae02f53664cd05eb54b560dc7d
ThatGremlin <ben.johnson@eagles.oc.edu>
2023-04-01 17:11:07 -0500

    readded [auth] to routes

--- app/routes/event.routes.js
+++ app/routes/event.routes.js
@@ -1,31 +1,33 @@
 module.exports = (app) => {
   const event = require("../controllers/event.controller.js");
-  // const { authenticate } = require("../authorization/authorization.js");
+  const { authenticate } = require("../authorization/authorization.js");
   var router = require("express").Router();
 
   // Create a new event
-  router.post("/", event.create);
+  router.post("/", [authenticate], event.create);
   // Retrieve all events
-  router.get("/", event.findAll);
+  router.get("/", [authenticate], event.findAll);
   // Retrieve a single event with id
-  router.get("/:id", event.findById);
+  router.get("/:id", [authenticate], event.findById);
   // Retrieve all events with date after date
-  router.get("/date/:date", event.findDateAndAfter);
+  router.get("/date/:date", [authenticate], event.findDateAndAfter);
   // Update a event with id
-  router.put("/:id", event.update);
+  router.put("/:id", [authenticate], event.update);
   // Delete a event with id
-  router.delete("/:id", event.delete);
+  router.delete("/:id", [authenticate], event.delete);
   // Delete all events
-  router.delete("/", event.deleteAll);
+  router.delete("/", [authenticate], event.deleteAll);
   // Retrieve timeslots fore current date
   router.get(
     "/critiqueTimeslots/:date",
+    [authenticate],
     event.getStudentTimeslotsForCurrentDate
   );
 
   // Retrieve critiques by semester id
   router.get(
     "/semesterCritiques/:semesterId",
+    [authenticate],
     event.getEventCritiquesBySemesterId
   );
 
