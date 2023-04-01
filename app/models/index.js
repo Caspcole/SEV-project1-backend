5214c25b69d4fd17879d43fd38c196756b2850a1
Andrew Clubb <andrew.clubb@eagles.oc.edu>
2023-04-01 12:38:36 -0500

    allow foreign key to be null

--- app/models/index.js
+++ app/models/index.js
@@ -120,8 +120,7 @@ db.event.belongsTo(db.semester);
 
 //EventTimeslot FKs
 db.userRole.hasMany(db.eventTimeslot, {
-  foreignKey: { name: "accompanistId", allowNull: false },
-  onDelete: "CASCADE",
+  foreignKey: { name: "accompanistId" },
 });
 db.event.hasMany(db.eventTimeslot);
 
