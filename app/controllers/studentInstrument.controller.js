17a2be3f68f39c3493f45d25ab97f38c4e587dc0
Andrew Clubb <andrew.clubb@eagles.oc.edu>
2023-04-04 01:18:35 -0500

    get instruments for logged in student

--- app/controllers/studentInstrument.controller.js
+++ app/controllers/studentInstrument.controller.js
@@ -7,38 +7,39 @@ exports.create = (req, res) => {
   // Validate request
   if (!req.body.studentId) {
     res.status(400).send({
-      message: "studentId can not be empty!"
+      message: "studentId can not be empty!",
     });
     return;
   } else if (!req.body.instrumentId) {
     res.status(400).send({
-      message: "instrumentId can not be empty!"
+      message: "instrumentId can not be empty!",
     });
     return;
   } else if (!req.body.instructorId) {
     res.status(400).send({
-      message: "instructorId can not be empty!"
+      message: "instructorId can not be empty!",
     });
     return;
   }
-  
+
   const studentInstrument = {
     level: req.body.level,
     studentId: req.body.studentId,
     instrumentId: req.body.instrumentId,
     accompanistId: req.body.accompanistId,
-    instructorId: req.body.instructorId
+    instructorId: req.body.instructorId,
   };
 
   // Create and Save a new studentInstrument
   StudentInstrument.create(studentInstrument)
-    .then(data => {
+    .then((data) => {
       res.send(data);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while creating the studentInstrument."
+          err.message ||
+          "Some error occurred while creating the studentInstrument.",
       });
     });
 };
@@ -46,13 +47,14 @@ exports.create = (req, res) => {
 // Retrieve all studentInstruments from the database
 exports.findAll = (req, res) => {
   StudentInstrument.findAll()
-    .then(data => {
+    .then((data) => {
       res.send(data);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while retrieving studentInstruments."
+          err.message ||
+          "Some error occurred while retrieving studentInstruments.",
       });
     });
 };
@@ -61,18 +63,18 @@ exports.findAll = (req, res) => {
 exports.findById = (req, res) => {
   const id = req.params.id;
   StudentInstrument.findByPk(id)
-    .then(data => {
+    .then((data) => {
       if (data) {
         res.send(data);
       } else {
         res.status(404).send({
-          message: 'Cannot find studentInstrument with id=' + id
+          message: "Cannot find studentInstrument with id=" + id,
         });
       }
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message: 'Error retrieving studentInstrument with id=' + id
+        message: "Error retrieving studentInstrument with id=" + id,
       });
     });
 };
@@ -81,48 +83,54 @@ exports.findById = (req, res) => {
 exports.update = (req, res) => {
   const id = req.params.id;
   StudentInstrument.update(req.body, {
-    where: { id: id }
+    where: { id: id },
   })
-  .then(num => {
-    if (num == 1) {
-      res.send({
-        message: 'StudentInstrument was updated successfully.'
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: "StudentInstrument was updated successfully.",
+        });
+      } else {
+        res.send({
+          message:
+            "Cannot update studentInstrument with id=" +
+            id +
+            ". Maybe the studentInstrument was not found or req.body is empty!",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error updating studentInstrument with id=" + id,
       });
-    } else {
-      res.send({
-        message: 'Cannot update studentInstrument with id=' + id + '. Maybe the studentInstrument was not found or req.body is empty!'
-      });
-    }
-  })
-  .catch(err => {
-    res.status(500).send({
-      message: 'Error updating studentInstrument with id=' + id
     });
-  });
 };
 
 // Delete a(n) studentInstrument with the specified id in the request
 exports.delete = (req, res) => {
   const id = req.params.id;
   StudentInstrument.destroy({
-    where: { id: id }
+    where: { id: id },
   })
-  .then(num => {
-    if (num == 1) {
-      res.send({
-        message: 'StudentInstrument was deleted successfully!'
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: "StudentInstrument was deleted successfully!",
+        });
+      } else {
+        res.send({
+          message:
+            "Cannot delete studentInstrument with id=" +
+            id +
+            ". Maybe the studentInstrument was not found",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Could not delete studentInstrument with id=" + id,
       });
-    } else {
-      res.send({
-        message: 'Cannot delete studentInstrument with id=' + id + '. Maybe the studentInstrument was not found'
-      })
-    }
-  })
-  .catch((err) => {
-    res.status(500).send({
-      message: "Could not delete studentInstrument with id=" + id,
     });
-  });
 };
 
 // Delete all studentInstruments from the database.
@@ -132,12 +140,48 @@ exports.deleteAll = (req, res) => {
     truncate: false,
   })
     .then((nums) => {
-      res.send({ message: `${nums} studentInstruments were deleted successfully!` });
+      res.send({
+        message: `${nums} studentInstruments were deleted successfully!`,
+      });
     })
     .catch((err) => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while removing all studentInstruments.",
+          err.message ||
+          "Some error occurred while removing all studentInstruments.",
+      });
+    });
+};
+
+exports.getByUserId = (req, res) => {
+  StudentInstrument.findAll({
+    include: [
+      {
+        model: db.userRole,
+        as: "student",
+        required: true,
+        include: {
+          model: db.user,
+          required: true,
+          where: {
+            id: { [Op.eq]: req.params.userId },
+          },
+        },
+      },
+      {
+        model: db.instrument,
+        required: true,
+      },
+    ],
+  })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message ||
+          "Some error occurred while retrieving studentInstruments.",
       });
     });
 };
